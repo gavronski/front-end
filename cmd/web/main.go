de9b44a3308749af71cc15a64745c7c0878ef0d6
gavronski <pawelgawronski08@gmail.com>
2023-01-31 23:16:37 +0100

    delete parameter for render func

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -23,56 +23,10 @@ type Note struct {
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		notes := []Note{
-			{
-				Name:            "Test1",
-				Description:     "Description1",
-				TextColor:       "#FDF6F2",
-				BackgroundColor: "#4466EE",
-			},
-			{
-				Name:            "Test1",
-				Description:     "Description1",
-				TextColor:       "#FDF6F2",
-				BackgroundColor: "#44EE47",
-			},
-			{
-				Name:            "Test1",
-				Description:     "Description1",
-				TextColor:       "#FDF6F2",
-				BackgroundColor: "#EE6644",
-			},
-			{
-				Name:            "Test1",
-				Description:     "Description1",
-				TextColor:       "#FDF6F2",
-				BackgroundColor: "#E83092",
-			},
-			{
-				Name:            "Test1",
-				Description:     "Description1",
-				TextColor:       "#FDF6F2",
-				BackgroundColor: "#4466EE",
-			},
-			{
-				Name:            "Test1",
-				Description:     "Description1",
-				TextColor:       "#FDF6F2",
-				BackgroundColor: "#9A961D",
-			},
-		}
-		render(w, "body.page.gohtml", notes)
+		render(w, "body.page.gohtml")
 	})
 
 	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
-
-		// var note Note = Note{
-		// 	Name:            "test",
-		// 	Description:     "test descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest descriptiontest description",
-		// 	TextColor:       "#FDF6F2",
-		// 	BackgroundColor: "#EE8044",
-		// }
-
 		render(w, "notes.page.gohtml")
 	})
 
@@ -87,7 +41,7 @@ func main() {
 	}
 }
 
-func render(w http.ResponseWriter, layout string, td any) {
+func render(w http.ResponseWriter, layout string) {
 	partials := []string{
 		"./cmd/web/templates/base.layout.gohtml",
 		"./cmd/web/templates/header.partial.gohtml",
@@ -105,7 +59,8 @@ func render(w http.ResponseWriter, layout string, td any) {
 		return
 	}
 
-	if err := tmpl.Execute(w, td); err != nil {
+	// nil - place for data - any
+	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
